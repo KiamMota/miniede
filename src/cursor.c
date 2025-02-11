d48e498eb6d710d6ff1364b5fe0402039338a93b
KiamMota <133913407+KiamMota@users.noreply.github.com>
2025-02-11 00:45:34 -0300

    ADDED: !

--- src/cursor.c
+++ src/cursor.c
@@ -9,14 +9,12 @@ n_int *cursory = NULL;
 
 int currentX(n_int *cursorx)
 {
-
     getyx(stdscr, *cursory, *cursorx);
-
     return *cursorx;
 }
 int currentY(n_int *cursory)
 {
     getyx(stdscr, *cursory, *cursorx);
-
     return *cursory;
 }
+
