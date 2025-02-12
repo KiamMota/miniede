2c32aa707a31a6a8915ec7c073dffc5711ed908c
Kiam <somentekiam@gmail.com>
2025-02-12 17:13:51 -0300

    ADD: macros of ANSI escape to mede.h ; DOC

--- include/mede.h
+++ include/mede.h
@@ -1,18 +1,45 @@
 #ifndef MEDE_H_INCLUDED
 #define MEDE_H_INCLUDED
 
-#include <stdint.>
+#include <stdint.h>
 
 //	this lib loads miniEDE own types, defs and macros.
 
 
 #define START_VAL 0
 #define und_ptr 0
+
 //typedefs to especific optimized types
 
 typedef signed short n_int; //  nano int defined with 2 bytes
 typedef int8_t nn_int;  //  nano nano int defined with 1 byte
 typedef uint8_t n_bool; //  nano bool defined with 1 byte
 typedef char* n_string; //  nano string
+
+//-----------------------//
+//	ESC ANSI	 //
+//-----------------------//
+
+#define ESC "\033["
+
+//CURSOR
+
+#define c_up "A"
+#define c_dw "B"
+#define c_lf "D"
+#define c_rg "C"
+
+#define c_hide "?25l"
+#define c_show "?25h"
+
+#define cursor_mv(nn_int, direc) ESC nn_int #direc
+#define cursor_vs(visibility) ESC #visibility
+
+
+
+
+
+
+
 
 #endif //nede.h
