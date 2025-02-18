9cfc76b60a7a6453c4f446839c9dbf59e8defa6a
KiamMota <133913407+KiamMota@users.noreply.github.com>
2025-02-18 02:43:23 -0300

    RM: movimentation logic ; ADD: 'checkNullKb()' in '_cursorMove()'

--- src/cursor.c
+++ src/cursor.c
@@ -8,36 +8,36 @@
 n_int cursy = START_VAL;
 n_int cursx = START_VAL;
 
-void _cursorMove(n_int *kb_input)
+void _cursorMove(int *kb_input)
 {
-	cursor_yx(cursy, cursx);
-	switch(*kb_input)
-	{
-		case K_UP:
-			cursor_mv(cursy - 1, cursx);
-			break;
-		case K_DOWN:
-			cursor_mv(cursy + 1, cursx);
-			break;
-		case K_LEFT:
-			cursor_mv(cursy, cursx - 1);
-			break;
-		case K_RIGHT:
-			cursor_mv(cursy, cursx + 1);
-			break;
-	}
+	checkNullKb(kb_input);
+		//TODO LOGIC OF MOVIMENTATION
 }
 
-Kb_Cursor kb_c = 
+void _changeCursor(int *kb_input)
 {
-	.cursorMove = _cursorMove
+	if(*kb_input == K_F1)
+	{
+		cursor_style(c_bar);
+	}else if(*kb_input == K_F2)
+	{
+		cursor_style(c_blocky);
+	}else if(*kb_input == K_F3)
+	{
+		cursor_style(c_bar);
+	}
+}
+Kb_Cursor kb_c =
+{
+	.cursorMove = _cursorMove,
+	.changeCursor = _changeCursor
 };
 
-void cursorMoveSet(n_int *kb_input)
+void initCursor(int *kb_input)
 {
 	while(1)
 	{
-		*kb_input = getchar();
+		kb_c.changeCursor(kb_input);
 		kb_c.cursorMove(kb_input);
 	}
 }
