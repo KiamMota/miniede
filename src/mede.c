5176327bdbf6a394c91ac07ebee36466c9c34947
KiamMota <133913407+KiamMota@users.noreply.github.com>
2025-02-19 22:52:43 -0300

    ADD: Windows setRawMode init logic... is not working...

--- src/mede.c
+++ src/mede.c
@@ -1,8 +1,15 @@
 #include "mede.h"
 
+#ifdef _WIN32
+    #include <windows.h>
+#elif __linux__ || __APPLE__
+    #include <terminos.h>
+#endif
+
 void mederun()
 {
     CLEAR_SCREEN();
+    setRawMode(1);
     cursor_vs(c_show);
     cursor_style(c_bar);
     cursor_mv(START_Y, START_X);
@@ -19,6 +26,41 @@ int byemede()
     msg("close safety");
     cursor_vs(c_show);
     cursor_style(c_bar);
+    setRawMode(0);
     return 0;
 }
 
+void setRawMode(nn_int freeFlag)
+{
+    #if defined(_WIN32)
+
+    // ***************************
+    // * W I N D O W S   M O D E *
+    // ***************************
+
+    HANDLE WIN_medeHandle = GetStdHandle(STD_INPUT_HANDLE);
+    LPDWORD WIN_medeConsoleState = (DWORD *)malloc(sizeof(DWORD));
+    if(GetConsoleMode(WIN_medeHandle, WIN_medeConsoleState))
+    {
+        SetConsoleMode(WIN_medeHandle, *WIN_medeConsoleState);
+        *WIN_medeConsoleState &= ~ENABLE_LINE_INPUT;
+    }else
+    {
+        printf("mede ERR(1): failed to set Windows raw mode!");
+        free(WIN_medeConsoleState);
+    }
+
+    switch(freeFlag)
+    {
+    case 0:
+        if(WIN_medeConsoleState != NULL)
+        {
+            free(WIN_medeConsoleState);
+            WIN_medeConsoleState = NULL;
+        }
+    break;
+    }
+
+
+    #endif
+}
